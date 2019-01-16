d5a91f4a8cb8ec3149a73913f2ba835a2dd2e3d8
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-16 17:48:44 -0600

    fix: Fixed exact error and cleaned styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,48 +9,49 @@ class NavBar extends Component {
     dispatch(setAuthedUser(null))
     this.props.history.push('/')
   }
-    render() {
-        return (
-          <nav className="navbar navbar-expand-sm">
-          <div className="container">
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm">
+        <div className="container">
           <div className="navbar-nav nav-tabs">
             <Link to='/leaderboard' 
               className="nav-link nav-item">
                     Leaderboard
+            </Link>    
+            <Link to="/" 
+              className="nav-link nav-item">
+                Home
+            </Link>  
+            <Link to='/add' 
+              className="nav-link nav-item">
+                Add Poll
             </Link>
-                
-                  <Link exact to="/" 
-                  className="nav-link nav-item">
-                    Home
-                  </Link>
-                
-                  <Link to='/add' 
-                  className="nav-link nav-item">
-                    Add Poll
-                  </Link>
-                {this.props.authedUser !== null
-                  ?
-                  <Link to='/Login' 
-                  className="nav-link nav-item">
+            {this.props.authedUser !== null
+              ?
+              <Link to='/Login' 
+                className="nav-link nav-item">
                   Logged in as {this.props.authedUser}
-                  </Link>
-                  :
-                  <Link to='/Login' 
+              </Link>
+              :
+              <Link to='/Login' 
                   className="nav-link nav-item">
                     Login
                   </Link>
-                }
-              
-            
+            }      
           </div>
-          {this.props.authedUser !== null && <button 
-            onClick={this.handleLogout}
-            className="btn btn-primary"
-            >Logout</button>}
-          </div>
-          </nav>
-        )
-    }
+          {this.props.authedUser !== null 
+            && 
+              <button 
+                onClick={this.handleLogout}
+                className="btn btn-primary">
+                  Logout
+              </button>
+          }
+        </div>
+      </nav>
+    )
+  }
 }
 
 function mapStateToProps ({ authedUser }){
