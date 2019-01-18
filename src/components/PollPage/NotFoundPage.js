2802b4062df160a5650c28659de9bda8ad2f39dc
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-18 12:28:40 -0600

    feat: Styled poll not found page

--- src/components/PollPage/NotFoundPage.js
+++ src/components/PollPage/NotFoundPage.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>404 page not found</h1>
+        <div className="display-component">
+            <h1 className="display-2 pb-4">404 page not found</h1>
+            <h2 className="display-4">The page you are looking for doesn't appear to exist. Either the url is incorrect or the poll id does not match those in our records</h2>
         </div>
     )
 }
