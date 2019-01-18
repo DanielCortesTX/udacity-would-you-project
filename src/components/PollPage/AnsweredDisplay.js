d98f80e0ce2cc70f38fdac84dc2d13f3c42684a1
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-18 12:22:00 -0600

    feat: Styled answered poll page

--- src/components/PollPage/AnsweredDisplay.js
+++ src/components/PollPage/AnsweredDisplay.js
@@ -14,42 +14,41 @@ class AnsweredDisplay extends Component {
         const { avatarURL } = this.props.userData
         const checkAnswerOne = (userAnswer) => {
             if(userAnswer){
-                return 'answer-box ans-selected'
+                return 'card card-body bg-dark text-white mb-2'
             } else {
-                return 'answer-box'
+                return 'card card-body mb-2'
             }
         }
         const checkAnswerTwo = (userAnswer) => {
             if(!userAnswer){
-                return 'answer-box ans-selected'
+                return 'card card-body bg-dark text-white mb-4'
             } else {
-                return 'answer-box'
+                return 'card card-body mb-4'
             }
         }
         const calcPercent = (votes, total) => {
             return ( votes / total ) * 100
         }
         return (
-            <div className="standard-boundaries">
-                <h2>Would you rather...</h2>
-                <div className="answer-user-display">
+            <div className="display-component">
+                <div className="card test p-4 mb-4">
                     <img
                         src={avatarURL}
                         alt={'?'}
                         className="avatar"
                     />
-                    <h4>{author} asked:</h4>
+                    <h1 className="display-4">{author} asked: Would you rather...</h1>
                 </div>
                 <div className={checkAnswerOne(userAnswer)}>
-                  <h5>{optionOne.text}</h5>
-                  <p>{optionOne.votes.length} out of {total} people voted for this. Or {calcPercent(optionOne.votes.length, total)}%</p>
-                  {userAnswer && <p>Including you.</p>}
+                  <h2 className="display-4">{optionOne.text}</h2>
+                  <p className="lead">{optionOne.votes.length} out of {total} people voted for this. Or {calcPercent(optionOne.votes.length, total)}%</p>
+                  {userAnswer && <p className="text-muted">Including you.</p>}
                 </div>
-                <h4>OR....</h4>
+                <h1 className="display-4">OR....</h1>
                 <div className={checkAnswerTwo(userAnswer)}>
-                  <h5>{optionTwo.text}</h5>
-                  <p>{optionTwo.votes.length} out of {total} people voted for this. Or {calcPercent(optionTwo.votes.length, total)}%</p>
-                  {!userAnswer && <p>Including you.</p>}
+                  <h2 className="display-4">{optionTwo.text}</h2>
+                  <p className="lead">{optionTwo.votes.length} out of {total} people voted for this. Or {calcPercent(optionTwo.votes.length, total)}%</p>
+                  {!userAnswer && <p className="text-muted">Including you.</p>}
                 </div>
             </div>
         )
