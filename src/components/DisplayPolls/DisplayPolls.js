7247b99fdf7613f7498b3662e50ff919b02c12ca
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-18 21:53:40 -0600

    style: Adjusted indentions on leaderboard and display polls files

--- src/components/DisplayPolls/DisplayPolls.js
+++ src/components/DisplayPolls/DisplayPolls.js
@@ -3,74 +3,77 @@ import { connect } from 'react-redux'
 import PollLink from './PollLink'
 
 class DisplayPolls extends Component {
-    state ={
-        unanswered: true
-    }
-    toggleView = () => {
-        /*
-          @description local state determines whether array of answered or unanswered
-            questions are displayed
-        */
-        this.setState((prevState) => ({
-            unanswered: !prevState.unanswered
-        }))
-    }
-    render() {
-        const { answeredQuestions, unansweredQuestions } = this.props
-        return (
-            <div className="container display-component">
-              <h1 className="display-4 App mb-4">Polls</h1>
+  state ={
+    unanswered: true
+  }
+  toggleView = () => {
+    /*
+      @description local state determines whether array of answered or unanswered
+        questions are displayed
+    */
+    this.setState((prevState) => ({
+      unanswered: !prevState.unanswered
+    }))
+  }
+  render() {
+    const { answeredQuestions, unansweredQuestions } = this.props
+    return (
+      <div className="container display-component">
+        <h1 className="display-4 App mb-4">Polls</h1>
 			  <button 
 				  onClick={this.toggleView}
-				  className="btn btn-primary mb-4">
-                {this.state.unanswered === true ? 'View answered Questions': 'View unanswered Questions'}
+					className="btn btn-primary mb-4"
+					>
+            {this.state.unanswered === true ? 'View answered Questions': 'View unanswered Questions'}
 			  </button>
 			  <div className="drop-trim">
-              {this.state.unanswered === true ?
-                unansweredQuestions.map((poll) => (
-                    <div key={poll.id} className="mb-4">
-                        <PollLink poll={poll}/>
-                    </div>
-                )) :
-                answeredQuestions.map((poll) => (
-                    <div key={poll.id} className="mb-4">
-                      <PollLink poll={poll}/>
-                    </div>
-                ))
-			  }
+					{this.state.unanswered === true
+						?
+            unansweredQuestions.map((poll) => (
+              <div key={poll.id} className="mb-4">
+                <PollLink poll={poll}/>
+              </div>
+						)) 
+						:		
+						answeredQuestions.map((poll) => (
+              <div key={poll.id} className="mb-4">
+                <PollLink poll={poll}/>
+							</div>
+						))
+			  	}
 			  </div>
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 function mapStateToProps ({ polls, authedUser }){
-    /*
-      @description polls are mapped to array by their id. That array is then mapped
-        into array of each poll's data. That array is then filtered to return one
-        array of questions answered by authedUser and another of questions unanswered
-        by authedUser.
-    */
-    const pollsArray = Object.keys(polls)
-      .sort((a,b) => polls[b].timestamp - polls[a].timestamp)
-    const questionsToFilter = pollsArray.map((poll) => {
-        const pollData = polls[poll]
-        return pollData
-    })
-    const answeredQuestions = questionsToFilter.filter((poll) =>{
-        return poll.optionOne.votes.includes(authedUser) || poll.optionTwo.votes.includes(authedUser)
-    })
-    const unansweredQuestions = questionsToFilter.filter((poll) =>{
-        return !poll.optionOne.votes.includes(authedUser) && !poll.optionTwo.votes.includes(authedUser)
-    })
-    const testUser = polls['8xf0y6ziyjabvozdd253nd']
-    return {
-        pollsId: pollsArray,
-        polls: questionsToFilter,
-        testUser,
-        answeredQuestions,
-        unansweredQuestions,
-    }
+  /*
+  	@description polls are mapped to array by their id. That array is then mapped
+      into array of each poll's data. That array is then filtered to return one
+      array of questions answered by authedUser and another of questions unanswered
+      by authedUser.
+  */
+  const pollsArray = Object.keys(polls)
+    .sort((a,b) => polls[b].timestamp - polls[a].timestamp)
+  const questionsToFilter = pollsArray.map((poll) => {
+  	const pollData = polls[poll]
+  	return pollData
+  })
+  const answeredQuestions = questionsToFilter.filter((poll) =>{
+  	return poll.optionOne.votes.includes(authedUser) || poll.optionTwo.votes.includes(authedUser)
+  })
+  const unansweredQuestions = questionsToFilter.filter((poll) =>{
+    return !poll.optionOne.votes.includes(authedUser) && !poll.optionTwo.votes.includes(authedUser)
+  })
+  const testUser = polls['8xf0y6ziyjabvozdd253nd']
+  return {
+    pollsId: pollsArray,
+    polls: questionsToFilter,
+    testUser,
+    answeredQuestions,
+    unansweredQuestions,
+  }
 }
 
 export default connect(mapStateToProps)(DisplayPolls)
--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -3,52 +3,52 @@ import { connect } from 'react-redux'
 import LeadUserDisplay from './LeadUserDisplay'
 
 class Dashboard extends Component {
-    componentDidUpdate(){
-        console.log(this.props.usersId)
-        console.log(this.props.users)
-    }
-    render() {
-        const { usersId } = this.props
-        return (
-            <div className="container display-component">
-                <h1 className="display-4 mb-4">Welcome to the leaderboard!</h1>
-                <div>
-                    {usersId.map((user, index) => {
-                        const id = user.id
-                        const userAnswers = Object.keys(user.userData.answers).length
-                        const userData = user.userData
-                        return (
-                            <div key={index}>
-                                <LeadUserDisplay key={id} userAnswers={userAnswers} userData={userData}/>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        )
-    }
+  componentDidUpdate(){
+    console.log(this.props.usersId)
+    console.log(this.props.users)
+  }
+  render() {
+    const { usersId } = this.props
+    return (
+      <div className="container display-component">
+        <h1 className="display-4 mb-4">Welcome to the leaderboard!</h1>
+        <div>
+          {usersId.map((user, index) => {
+            const id = user.id
+            const userAnswers = Object.keys(user.userData.answers).length
+            const userData = user.userData
+            return (
+              <div key={index}>
+                <LeadUserDisplay key={id} userAnswers={userAnswers} userData={userData}/>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps ({ users }){
-    /*
-      @description object of user ids are extracted from store. ids are used
-        to return an array of objects that have the sum of the users questions asked and 
-        quetions answered, the user id and the user's data. the objects are then sorted
-        by the score.
-    */
-    const usersIds = Object.keys(users)
-    const sortTotals = usersIds.map((userId) => {
-        const userData = users[userId]
-        const answerLength = Object.keys(userData.answers).length
-        const total = answerLength + userData.questions.length
-        return { 'userScore': total, 'user': userId, 'userData': userData }
-    })
-    return {
-        usersId: sortTotals.sort((a,b) => {
-            return b.userScore - a.userScore
-        }),
-        users: usersIds
-    }
+  /*
+    @description object of user ids are extracted from store. ids are used
+      to return an array of objects that have the sum of the users questions asked and 
+      quetions answered, the user id and the user's data. the objects are then sorted
+      by the score.
+  */
+  const usersIds = Object.keys(users)
+  const sortTotals = usersIds.map((userId) => {
+  	const userData = users[userId]
+  	const answerLength = Object.keys(userData.answers).length
+  	const total = answerLength + userData.questions.length
+  	return { 'userScore': total, 'user': userId, 'userData': userData }
+  })
+  return {
+    usersId: sortTotals.sort((a,b) => {
+      return b.userScore - a.userScore
+    }),
+    users: usersIds
+  }
 }
 
 export default connect(mapStateToProps)(Dashboard)
