08a22022210487fb72e58ac01aa5d27c85f586d8
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-20 13:00:25 -0600

    feat: Added to home link on not found page for UX

--- src/components/PollPage/NotFoundPage.js
+++ src/components/PollPage/NotFoundPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
   return (
     <div className="display-component">
       <h1 className="display-2 pb-4">404 page not found</h1>
 			<h2 className="display-4">The page you are looking for doesn't appear to exist. Either the url is incorrect or the poll id does not match those in our records
-			</h2>
+      </h2>
+      <Link to="/" className="btn btn-primary">Return to Polls feed</Link>
     </div>
   )
 }
