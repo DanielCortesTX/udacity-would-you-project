ba5b4c51706e13ba24927e4fa9454e78315ab7ff
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-20 13:16:12 -0600

    refactor: Made PollLink a stateless functional component

--- src/components/DisplayPolls/PollLink.js
+++ src/components/DisplayPolls/PollLink.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-class PollDisplay extends Component {
-  /*
-  	@description poll options are trimmed and displayed. each is a link to
-      display the polls
-    */
+export default class test extends Component {
   render() {
     const { id, optionOne, optionTwo } = this.props.poll
     const teaseOne = optionOne.text.substring(0, 10)
@@ -20,10 +16,38 @@ class PollDisplay extends Component {
           className="btn btn-primary btn-lg"
         >
           View full Poll
-        </Link>              
+        </Link>
       </div>
     )
   }
 }
 
-export default PollDisplay
+// import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
+
+// class PollDisplay extends Component {
+//   /*
+//   	@description poll options are trimmed and displayed. each is a link to
+//       display the polls
+//     */
+//   render() {
+//     const { id, optionOne, optionTwo } = this.props.poll
+//     const teaseOne = optionOne.text.substring(0, 10)
+//     const teaseTwo = optionTwo.text.substring(0, 10)
+//     return (
+//       <div className="card card-body card-preview">
+//         <h5 className="">Would you rather...</h5>
+//         <p>{teaseOne}......</p>
+//         <p>{teaseTwo}......</p>
+//         <Link 
+//           to={`/questions/${id}`}
+//           className="btn btn-primary btn-lg"
+//         >
+//           View full Poll
+//         </Link>              
+//       </div>
+//     )
+//   }
+// }
+
+// export default PollDisplay
