906bb99f41d0ddc09953b582e7764d1d9a97ae7f
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-22 14:09:02 -0600

    refactor: Made LeadUserDisplay a functional component

--- src/components/Leaderboard/LeadUserDisplay.js
+++ src/components/Leaderboard/LeadUserDisplay.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class LeadUserDisplay extends Component {
-  /*
-    @description each display accepts user data and displays necessary particulars.
-  */
-  render() {
-    const { userData, userAnswers } = this.props
-    const { name, avatarURL } = userData
-    const question = userData.questions.length
-    const total = question + userAnswers
-    return (
-      <div className="card test full-trim mb-4">
-        <div className="card-body">
-          <img
-            src={avatarURL}
-            alt={'?'}
-            className="avatar"
-          />
-        </div>
-        <div className="card-body">
-          <h1 className="display-4">{`${name} has:`}</h1>
-          <p className="lead">{`${question} question(s) and ${userAnswers} answer(s)`}</p>
-        </div>
-				<div className="card-body bg-dark text-white col-3">
-					<h4 className="display-4">
-						{`Score: ${total}`}
-					</h4>
-				</div>
+const LeadUserDisplay = ({userData, userAnswers}) => {
+  const { name, avatarURL } = userData
+  const question = userData.questions.length
+  const total = question + userAnswers
+  return (
+    <div className="card test full-trim mb-4">
+      <div className="card-body">
+        <img
+          src={avatarURL}
+          alt={'?'}
+          className="avatar"
+        />
       </div>
-    )
-  }
-} 
+      <div className="card-body">
+        <h1 className="display-4">{`${name} has:`}</h1>
+        <p className="lead">{`${question} question(s) and ${userAnswers} answer(s)`}</p>
+      </div>
+			<div className="card-body bg-dark text-white col-3">
+				<h4 className="display-4">
+					{`Score: ${total}`}
+				</h4>
+			</div>
+    </div>
+  )
+}
 
 export default LeadUserDisplay
